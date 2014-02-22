28e46d856d6b2935280985c111f68c7c7532f477
Tommy Zng <1319524930@qq.com>
2014-02-22 17:42:51 +0800

    Create numberIterators.js

---
+++ numberIterators.js
@@ -0,0 +1,11 @@
+//this is a demo of iterator.js
+//but the version iterator.js is now at cannot
+//reach the goal
+//so I am developing a better solution
+Number.iterator(function(){
+  var up=this.valueOf();
+  for(var i=0;i<up;i++){
+    this.yield({n:i});
+  }
+  return null;
+}
