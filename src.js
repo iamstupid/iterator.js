091cecaf4331aa00fc12cdcf46fd34543a0f233a
Tommy Zng <1319524930@qq.com>
2014-02-23 12:06:10 +0800

    Add class iterator support

--- src.js
+++ src.js
@@ -11,23 +11,31 @@
 		}
 	}
 
-	function iterator_former(fn) {
-		this.fn = fn;
-		this.data = new pullData();
-		this.yield = function(pairs) {
+	function iterator_former(fn,x) {
+		x.fn = fn;
+		x.data = new pullData();
+		x.yield = function(pairs) {
 			this.data.data = dup(pairs);
 			return this.fn.call(this.data);
 		}
+		return x;
 	}
 
 	function iterator(fn){
 		return function(args,funct){
-			var block=new iterator_former(funct);
-			fn.call(block,args);
+			var block=iterator_former.call(this,funct,this);
+			return fn.call(block,args);
 		}
 	}
 
 	window.iterator=function(fn){
 		return iterator(fn);
+	}
+	
+	Function.prototype.iterator=function(fn,name){
+		this.prototype[name]=function(args,funct){
+			var a=iterator(fn);
+			return a.call(this,args,funct);
+		}
 	}
 })();
