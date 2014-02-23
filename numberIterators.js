2fe9c06695ba01b1aa1f451177c0bc2939574677
Tommy Zng <1319524930@qq.com>
2014-02-23 12:12:18 +0800

    Update numberIterators.js
    Add `upto` and `downto`

--- numberIterators.js
+++ numberIterators.js
@@ -5,6 +5,20 @@ Number.iterator(function(){
   }
   return null;
 },"times");
+Number.iterator(function(upTo){
+  var low=this.valueOf();
+  for(var i=low;i<=upTo;i++){
+    this.yield({n:i});
+  }
+  return null;
+},"upto");
+Number.iterator(function(downTo){
+  var high=this.valueOf();
+  for(var i=high;i>=downTo;i--){
+    this.yield({n:i});
+  }
+  return null;
+},"downto");
 /**
  * Ruby-style times iterator for Number s.
  * */
