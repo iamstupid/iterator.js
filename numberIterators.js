4c2a56718b746f6d516d02502c70cf91b4a30238
Tommy Zng <1319524930@qq.com>
2014-02-27 19:07:37 +0800

    Update numberIterators.js

--- numberIterators.js
+++ numberIterators.js
@@ -1,23 +1,35 @@
 Number.iterator(function(){
-  var up=this.valueOf();
-  for(var i=0;i<up;i++){
-    this.yield({n:i});
-  }
-  return null;
+	var up=this.valueOf();
+	if(this.hasBlock()){
+		for(var i=0;i<up;i++){
+			this.yield({n:i});
+		}
+	}else{
+		return new Iterator(function(){
+			var up=this.pull("up");
+			this.data.last++;
+			if(this.data.last>up){
+				this.end();
+			}else{
+				return this.data.last;
+			}
+		},{last:0,up:up});
+	}
+	return null;
 },"times");
 Number.iterator(function(upTo){
-  var low=this.valueOf();
-  for(var i=low;i<=upTo;i++){
-    this.yield({n:i});
-  }
-  return null;
+	var low=this.valueOf();
+	for(var i=low;i<=upTo;i++){
+		this.yield({n:i});
+	}
+	return null;
 },"upto");
 Number.iterator(function(downTo){
-  var high=this.valueOf();
-  for(var i=high;i>=downTo;i--){
-    this.yield({n:i});
-  }
-  return null;
+	var high=this.valueOf();
+	for(var i=high;i>=downTo;i--){
+		this.yield({n:i});
+	}
+	return null;
 },"downto");
 /**
  * Ruby-style times iterator for Number s.
